4a846a6380b33ed029888f230b04ca7aac034991
Мария Клепикова <masha.k2001@gmail.com>
2024-04-08 15:25:48 +0300

    [feat] при запуске приложения вызывается метод, инициализирующий поля CurrentWeatherViewModel

--- WeatherWpfApp/App.xaml.cs
+++ WeatherWpfApp/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using WeatherWpfApp.ViewModels;
 
 namespace WeatherWpfApp;
 
@@ -9,4 +10,17 @@ namespace WeatherWpfApp;
 /// </summary>
 public partial class App : Application
 {
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		base.OnStartup(e);
+
+		var currentWeatherViewModel = new CurrentWeatherViewModel();
+
+		currentWeatherViewModel.Initialize();
+
+		new MainWindow()
+		{
+			DataContext = currentWeatherViewModel,
+		}.Show();
+	}
 }
