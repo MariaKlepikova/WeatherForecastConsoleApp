c6760f4f31799d9f912cb15105ba59d6fc909e41
Мария Клепикова <masha.k2001@gmail.com>
2024-04-08 15:52:24 +0300

    [add] enum Precipitation

---
+++ WeatherForecastLibrary/Models/Precipitation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecastLibrary.Models
+{
+	public enum Precipitation
+	{
+		Cloudy,
+		PartlyCloudy,
+		Clearly,
+		Shower,
+		Rain,
+		Snow
+	}
+}
