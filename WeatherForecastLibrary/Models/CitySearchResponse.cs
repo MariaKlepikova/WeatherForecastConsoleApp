28d46236630a08b6849219b8c4f9a57c555b78f8
Мария Клепикова <masha.k2001@gmail.com>
2024-04-08 17:17:37 +0300

    [add] CitySearchResponse.cs

---
+++ WeatherForecastLibrary/Models/CitySearchResponse.cs
@@ -0,0 +1,17 @@
+using System.Text.Json.Serialization;
+namespace WeatherForecastLibrary.Models;
+
+public class CitySearchResponse
+{
+    [JsonPropertyName("lat")]
+    public string Latitude { get; set; }
+
+    [JsonPropertyName("lon")]
+    public string Longitude { get; set; } 
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("display_name")]
+    public string DisplayName { get; set; }
+}
